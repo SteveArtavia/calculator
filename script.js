d806d31d304c804f5503ef49ed86f198d647ceec
Steve <stvartavia@gmail.com>
2024-07-23 15:25:18 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
 let display = document.getElementById('display');
 
-const zero = 0;
-const one = 1;
-const two = 2;
-const three = 3;
-const four = 4;
-const five = 5;
-const six = 6;
-const seven = 7;
-const eight = 8;
-const nine = 9;
-
-const division = '/';
-const multiplier = '*';
-const minus = '-';
-const plus = '+';
-const equals = '=';
-
 const clearBtn = document.getElementById('clearBtn')
 const zeroBtn = document.getElementById('zero');
-const oneBtn = document.documentElement('one');
+const oneBtn = document.getElementById('one');
 const twoBtn = document.getElementById('two');
 const threeBtn = document.getElementById('three');
 const fourBtn = document.getElementById('four');
@@ -35,12 +18,9 @@ const minusBtn = document.getElementById('minus');
 const plusBtn = document.getElementById('plus');
 const equalsBtn = document.getElementById('equals');
 
-let btn = document.querySelectorAll('button');
-
-let operand = undefined;
-
-btn.addEventListener('click', () => {
-    display.appendChild('p').textContent = btn.value;
+oneBtn.addEventListener('click', () => {
+    display.value = '1';
 })
+
 
 
